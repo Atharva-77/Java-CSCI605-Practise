4bb80b6916ba678dafddc4af3a101827681d813a
Atharva-77 <atharva.shirode99@gmail.com>
2023-10-11 16:35:57 -0400

    See type in generics class code

--- Generics/GenericsDemo1.java
+++ Generics/GenericsDemo1.java
@@ -13,7 +13,10 @@ class Container<T>
 
 public class GenericsDemo1 {
     public static void main(String[] args) {
-        Container<?> obj= new Container<>("20");
+        Container<?> obj= new Container<>("20"); // can put any value
         obj.show();
+
+        Container<Integer> obj1= new Container<>(20); // only integer allowed...type safety
+        obj1.show();
     }
 }
