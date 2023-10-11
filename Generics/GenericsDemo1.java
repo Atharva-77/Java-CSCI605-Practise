9b99e90cdf02c3bef94797da35d426a9685cb111
Atharva-77 <atharva.shirode99@gmail.com>
2023-10-11 16:21:23 -0400

    Add without generics class object code

--- Generics/GenericsDemo1.java
+++ Generics/GenericsDemo1.java
@@ -6,6 +6,6 @@ class Container<T>
 
 public class GenericsDemo1 {
     public static void main(String[] args) {
-        Container <?>obj= new Container<>();
+        Container obj= new Container();
     }
 }
