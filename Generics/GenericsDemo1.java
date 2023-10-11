a259591fbe8455809d144e88a9fd398ad96cc354
Atharva-77 <atharva.shirode99@gmail.com>
2023-10-11 16:29:35 -0400

    See type in generics class code

--- Generics/GenericsDemo1.java
+++ Generics/GenericsDemo1.java
@@ -1,11 +1,19 @@
 class Container<T>
 {
     T value;
+    Container(T value){
+        this.value=value;
+    }
+
+    public void show(){
+        System.out.println("Value= "+value+" "+value.getClass().getName());
+    }
 }
 
 
 public class GenericsDemo1 {
     public static void main(String[] args) {
-        Container obj= new Container();
+        Container<?> obj= new Container<>("20");
+        obj.show();
     }
 }
