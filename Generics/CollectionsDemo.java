80c5f3d9d32311bcd1417f99f3b1b42afe87174b
Atharva-77 <atharva.shirode99@gmail.com>
2023-10-11 21:49:32 -0400

    Custom Comparator interface with Student Class implemented

--- Generics/CollectionsDemo.java
+++ Generics/CollectionsDemo.java
@@ -2,19 +2,42 @@ import java.util.*;
 
 public class CollectionsDemo {
     public static void main(String[] args) {
-        List <Integer> values= new ArrayList<>(); //dynamic array using collections, arraylist
+        List <Student> values= new ArrayList<>(); //dynamic array using collections, arraylist
 
-        values.add(123);
-        values.add(56);
-        values.add(160);
+        values.add(new Student(1,23));
+        values.add(new Student(2,89));
+        values.add(new Student(3, 75));
 
-        values.add(1,15);
-        Comparator<Integer> comp=new CustomComparatorClass();
+        values.add(3, new Student(4,99));
+        Comparator<Student> comp=new CustomComparatorStudentClass();
 
         Collections.sort(values,comp);
 
-        for(int i: values){
+        for(Student i: values){
             System.out.println(i);
         }
+    }
+}
+
+class CustomComparatorStudentClass implements Comparator<Student>{
+    @Override
+    public int compare(Student o1, Student o2) {
+        if (o1.marks>o2.marks)
+            return -1;
+        return 1;
+    }
+}
+class Student{
+
+    int rollNo;
+    int marks;
+
+    Student(int rollNo, int marks){
+        this.rollNo=rollNo;
+        this.marks=marks;
+    }
+
+    public String toString(){
+        return "Student= "+rollNo+ ", Marks= "+marks;
     }
 }
