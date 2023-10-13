08fd3ea8b476c5a289881361b08b7b6ad1b2643b
Atharva-77 <atharva.shirode99@gmail.com>
2023-10-13 16:53:58 -0400

    Add different elements : Comparable

--- Generics/CollectionsDemo.java
+++ Generics/CollectionsDemo.java
@@ -2,31 +2,25 @@ import java.util.*;
 
 public class CollectionsDemo {
     public static void main(String[] args) {
-        List <Student> values= new ArrayList<>(); //dynamic array using collections, arraylist
+        List  values= new ArrayList(); //dynamic array using collections, arraylist
 
         values.add(new Student(1,23));
         values.add(new Student(2,89));
         values.add(new Student(3, 75));
 
         values.add(3, new Student(4,99));
+        values.add(2);
+
 //        Comparable<Student> comp=new Student();
 
-        Collections.sort(values);
+//        Collections.sort(values);
 
-        for(Student i: values){
+        for(Object i: values){
             System.out.println(i);
         }
     }
 }
-class CustomComparatorStudentClass implements Comparator<Student>{
-    @Override
-    public int compare(Student o1, Student o2) {
-        if (o1.marks>o2.marks)
-            return -1;
-        return 1;
-    }
-}
-class Student implements Comparable<Student>{
+class Student implements Comparable{
 
     int rollNo;
     int marks;
@@ -40,9 +34,9 @@ class Student implements Comparable<Student>{
         return "Student= "+rollNo+ ", Marks= "+marks;
     }
 
-    public int compareTo(Student o1) {
-        if (this.marks>o1.marks)
-            return -1;
-        return 1;
+    @Override
+    public int compareTo(Object o) {
+        System.out.println("Hello");
+        return 0;
     }
 }
