d4f9f14e4cc7684566d8e4bbae8f4069636b7453
Atharva-77 <atharva.shirode99@gmail.com>
2023-10-16 20:56:50 -0400

    8.14, modified,List  behaviour

--- Generics/8.14/UpperBoundExample.java
+++ Generics/8.14/UpperBoundExample.java
@@ -18,37 +18,23 @@ public class UpperBoundExample<T> {
         // linked list op UpperBoundExample->Square
         List<UpperBoundExample<Cube>> bList = new LinkedList<UpperBoundExample<Cube>>();
 
-        List<UpperBoundExample<? super Cube>> cList = new LinkedList<UpperBoundExample<? super Cube>>();
-        List<UpperBoundExample<? extends Cube>> dList = new LinkedList<UpperBoundExample<? extends Cube>>();
+        List<UpperBoundExample<? extends Cube>> cList = new LinkedList<UpperBoundExample<? extends Cube>>();
+
+        // aList.add(new UpperBoundExample<Cube>(new Cube()));
+        // aList.add(new UpperBoundExample<Brick>(new Cube()));
+        // aList.add(new UpperBoundExample(new Cube()));
+        aList.add(new UpperBoundExample(new Brick()));
 
 
-        aList.add( new UpperBoundExample<Brick>(new Brick() ) );
-        aList.add( new UpperBoundExample(new Cube() ) );
-        aList.add( new UpperBoundExample(new Square() ) );
+
+        // cList.add( new UpperBoundExample(new Cube() ) );
+        // cList.add( new UpperBoundExample(new Brick() ) );
+        // cList.add( new UpperBoundExample(new Square() ) );
 
 
-        bList.add( new UpperBoundExample<Cube>(new Cube() ) );
-        // cList.add( new UpperBoundExample<Square>(new Square() ) );
-        // cList.add( new UpperBoundExample<Brick>(new Brick() ) );
-        cList.add( new UpperBoundExample(new Cube() ) );
-        cList.add( new UpperBoundExample(new Brick() ) );
-        cList.add( new UpperBoundExample(new Square() ) );
-
-        dList.add( new UpperBoundExample(new Cube() ) );
-        dList.add( new UpperBoundExample(new Brick() ) );
-        dList.add( new UpperBoundExample(new Square() ) );
-
-        for(UpperBoundExample i: cList){
+        for(UpperBoundExample i: aList){
             System.out.println(i);
         }
-        // UpperBoundExample<Square>  qSquare  = new UpperBoundExample<Square>(new Square());
-	    
-/*
-	    cList.add( new UpperBoundExample<Square>(new Suare() ) );
-UpperBoundExample.java:26: error: incompatible types: UpperBoundExample<Square> cannot be converted to UpperBoundExample<? extends Cube>
-	    cList.add( new UpperBoundExample<Square>(new Suare() ) );
-	               ^
-*/
 
     }
 }
